73f08ee8e7ff64a253ab8d1e037a7ef73997b38f
unknown <lubos.lenco@gmail.com>
2018-05-14 23:22:46 +0200

    Init

---
+++ Sources/arm/MyTrait.hx
@@ -0,0 +1,24 @@
+package arm;
+
+import tf.TF;
+import tf.TFHelper;
+
+class MyTrait extends iron.Trait {
+	public function new() {
+		super();
+
+		notifyOnInit(function() {
+			TFHelper.init(function() {
+				var model = TF.sequential();
+				model.add(TF.layers.dense({units: 1, inputShape: [1]}));
+				model.compile({loss: 'meanSquaredError', optimizer: 'sgd'});
+				var xs = TF.tensor2d([1, 2, 3, 4], [4, 1]);
+				var ys = TF.tensor2d([1, 3, 5, 7], [4, 1]);
+				model.fit(xs, ys).then(function() {
+					var res = model.predict(TF.tensor2d([5], [1, 1]));
+					trace(res);
+				});
+			});
+		});
+	}
+}
